152228755829105ceb2d4be2dc399ad5ea6e9dc8
Marcel Caroly <mcaroly@gmail.com>
2017-10-08 07:29:22 +1100

    Make sure defaultTyping is disabled to eliminate runtime execution vulnerabilities

--- src/main/java/info/maccac/recorder/service/Main.java
+++ src/main/java/info/maccac/recorder/service/Main.java
@@ -1,5 +1,6 @@
 package info.maccac.recorder.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ratpack.handling.Handler;
 import ratpack.http.Status;
 import ratpack.server.RatpackServer;
@@ -8,6 +9,7 @@ public class Main {
     public static void main(String... args) throws Exception {
         RatpackServer.start(server -> server
                 .serverConfig(c -> c.port(8080))
+                .registryOf(r -> r.add(ObjectMapper.class, new ObjectMapper().disableDefaultTyping()))
                 .handlers(chain -> chain
                         .post("results", handlePostedResults())
                 )
